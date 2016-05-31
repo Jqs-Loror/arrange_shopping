bf2cba24cca6cb69923d2cdf3440b4538df4bf52
Ishank Gupta <ishank_18@yahoo.com>
2016-05-31 19:36:46 +0530

    Added public folder as public

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,7 +43,7 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/public/spree #{release_path}/public/spree"
+    run "ln -s #{shared_path}/public/spree #{release_path}/public/spree"
   end
 
   after "deploy:finalize_update", "deploy:symlink_config"
